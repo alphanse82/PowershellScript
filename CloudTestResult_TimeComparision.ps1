aaf211ad6763236d5707f5a27f91d707e177a86e
alphanse82 <RakeshKumarGupta@outlook.com>
2019-10-25 00:22:50 -0700

    comparing multiple Test Session

--- CloudTestResult_TimeComparision.ps1
+++ CloudTestResult_TimeComparision.ps1
@@ -2,69 +2,57 @@
 ##
 # Declaration 
 ##
-$sessionIdForParallelExecution = "7332bf6c-3974-b9fa-59a8-ed5b9954de09"
-$sessionIdForBaseExecution = "ad5cf73f-38f2-a9ed-ab5d-bece8d62d576"
-$ResultFilter = "All" # All Passed Failed
+
+$sessionIdForParallelExecutions = "6454164_a3923f95-3d6d-016e-d8eb-12df9cd4b82e","6508005_7332bf6c-3974-b9fa-59a8-ed5b9954de09","6559829_4e8d80c9-9993-5afa-1ad4-afee7a7b89f2", "6584338_d342c8f8-f467-9c2f-2591-ab9080a7a64f"
+$sessionIdForBaseExecution = "6580786_7f014bd9-1df7-7bea-e26e-185e44b72865"
 $ResultPath = "P:\POC\result"
-$debug = $false
 
 
 #####################################################################################################
 # script code
-
 
 function fetchCloutTestResult
 {
     param (
         [Parameter(Mandatory=$true)]
         [string] $sessionId,
-        [Parameter(Mandatory=$true)]
-        [bool] $debug = 0,
-        [Parameter(Mandatory=$true)]
-        [bool] $tenant = "CosmosDB3"
+        [bool] $debugFlag = 0,
+        [string] $tenant = "CosmosDB3"
     )
     
     $baseUrl = "https://cloudtest/api/tenants/{0}/sessions".Replace("{0}",$tenant)
 
     $sessionUrl = "$baseUrl/{0}/jobs".Replace("{0}", $sessionId)
-    $testJobDetailsURL = "$baseUrl/{0}/jobs/{1}/executions/{2}/cases"
     
-
-    if($debug -eq $true) {
+    if($debugFlag -eq $true) {
         Write-Host  "CloudTest Session URL ->  $sessionUrl"
     }
 
     $response = Invoke-WebRequest -Uri $sessionUrl
     $jsonTestJobs = ConvertFrom-Json $([String]::new($response.Content))
 
-    if($debug -eq $true) {
+    if($debugFlag -eq $true) {
         Write-Host "Job Counts are $($jsonTestJobs.Count)"
-        $jsonTestJobs | ConvertTo-Json | Out-File "P:\POC\result\$sessionId-1.log" -Force
     }
 
     $testCaseExecutionLog = @();
 
     Write-Host "Counts are $($jsonTestJobs.Count)"
+
     foreach ($jasonTestJob in $jsonTestJobs) 
     {
-        $newUrl = $testJobDetailsURL.Replace("{0}","$sessionId").Replace("{1}","$($jasonTestJob.JobId)").Replace("{2}","$($jasonTestJob.ExecutionId)");
-        
-        Write-Host $newUrl
 
-        $responseForTestJob = Invoke-WebRequest -Uri $newUrl
-        $jsonTestJob = ConvertFrom-Json $([String]::new($responseForTestJob.Content))
-        
         if($debug -eq $true) {
+            Write-Host ""
             Write-Host "TestJob -> $($jasonTestJob.ExecutionId) -> $($jasonTestJob.JobId) -> $($jasonTestJob.ExecutionDurationInMS)"
             Write-Host "Total Job Counts are $($jsonTestJob.Count)"
+            Write-Host ""
         }
 
         $tcresult = New-Object -TypeName PSObject -Property ([Ordered]@{
             TestJobName   = $($jasonTestJob.JobName);
             TestJobResult = "$($jasonTestJob.Result)";  
             TestJobDuration = [System.Math]::Round(( $jasonTestJob.SetupDurationInMS + $jasonTestJob.ExecutionDurationInMS ) / 60000, 2) ;
-            # SetupDurationInMS = $jasonTestJob.SetupDurationInMS;
-            # ExecutionDurationInMS = $jasonTestJob.ExecutionDurationInMS;
             NumOfPassedTestCases = $jasonTestJob.NumOfPassedTestCases;
             NumOfFailedTestCases = $jasonTestJob.NumOfFailedTestCases;
             })
@@ -76,46 +64,40 @@ function fetchCloutTestResult
     return $testCaseExecutionLog;
 }
 
-$testCaseExecutionLogModified = fetchCloutTestResult $sessionIdForParallelExecution 
-$testCaseExecutionLogModified | ConvertTo-Json | Out-File ( Join-Path $ResultPath "Modified.log") -Force
-$testCaseExecutionLogModified | Export-Csv -Path ( Join-Path $ResultPath "Modified.csv") -NoTypeInformation
 
-
-$testCaseExecutionLogBase = fetchCloutTestResult $sessionIdForBaseExecution 
+$testCaseExecutionLogBase = fetchCloutTestResult -sessionId ($sessionIdForBaseExecution -split "_")[1] -debug 0 -tenant "CosmosDB3"
 $testCaseExecutionLogBase | ConvertTo-Json | Out-File ( Join-Path $ResultPath "Base.log") -Force
-$testCaseExecutionLogBase | Export-Csv -Path ( Join-Path $ResultPath "Base.csv") -NoTypeInformation
 
+$testCaseExecutionLogBase =  $testCaseExecutionLogBase | Sort-Object  -Property  TestJobName | Select-Object @{label="TestJobName"; Expression= {$_.TestJobName}} , @{label="BaseDuration"; Expression= {$_.TestJobDuration}}
 
-# return 
-# $testCaseExecutionLogBase =  Get-Content -Raw -Path ( Join-Path $ResultPath "Base.log") | ConvertFrom-Json
-# $testCaseExecutionLogModified =  Get-Content -Raw -Path ( Join-Path $ResultPath "Modified.log" ) | ConvertFrom-Json
+$testCasesExecutionResults = $testCaseExecutionLogBase;
 
-$testCasesexeSummaryClasswise = @();
+foreach($parallelExecutionSessionId in $sessionIdForParallelExecutions) {
 
-$uniqueTestJob =  $testCaseExecutionLogModified | Sort-Object  -Property  TestJobName
+    $sessionId = ($parallelExecutionSessionId -split "_")[1];
+    $devOpsBuild = ($parallelExecutionSessionId -split "_")[0];
+    $testCaseExecutionLogModified = fetchCloutTestResult -sessionId $sessionId -debug 0 -tenant "CosmosDB3"
+    $testCaseExecutionLogModified | ConvertTo-Json | Out-File ( Join-Path $ResultPath "$($devOpsBuild)_Modified.log") -Force
+    
+    $testCasesexeSummaryClasswise = @();
 
-if( $ResultFilter -ne "All" ) {
-    $uniqueTestJob =  $testCaseExecutionLogModified | Where-Object TestJobResult -eq $ResultFilter | Sort-Object  -Property  TestJobName
+    foreach($testjob in $testCasesExecutionResults) {
+
+        $BasecaseResult = $testCasesExecutionResults | Where-Object TestJobName -eq $testjob.TestJobName
+        $ModifiedCaseResult = $testCaseExecutionLogModified | Where-Object TestJobName -eq $testjob.TestJobName  
+        $improvement =  [System.Math]::Round(100 *( $($BasecaseResult.BaseDuration) - $($ModifiedCaseResult.TestJobDuration)) / $($BasecaseResult.BaseDuration),2)   
+        $testCasesescw = $BasecaseResult | Select-Object *,@{label="$($devOpsBuild)_Result"; Expression= {$ModifiedCaseResult.TestJobResult}},@{label="$($devOpsBuild)_Duration"; Expression= {$ModifiedCaseResult.TestJobDuration}},@{label="$($devOpsBuild)_%Improvement"; Expression= {$improvement}}    
+    
+        $testCasesexeSummaryClasswise += $testCasesescw
+    }
+
+    $testCasesExecutionResults = $testCasesexeSummaryClasswise;
 }
 
-foreach($testjob in $uniqueTestJob) {
+$testCasesExecutionResults | ConvertTo-Json | Out-File ( Join-Path $ResultPath "Staging.log") -Force
+$Staging  =  Get-Content -Raw -Path ( Join-Path $ResultPath "Staging.log") | ConvertFrom-Json
+$fileName = (Get-Date -format s).Replace("-","").Replace(":","")
+$fileName = Join-Path $ResultPath "FinalComparision_$($fileName).csv"
+$Staging | Export-Csv $fileName -NoTypeInformation -Force
 
-    $BaseResult = $testCaseExecutionLogBase | Where-Object TestJobName -eq $testjob.TestJobName
-
-    $testCasesescw = New-Object -TypeName PSObject -Property ([Ordered]@{
-        "TestJobName"   = $($testJob.TestJobName);
-        "TestJobResult" = "$($testJob.TestJobResult)";
-        "BeforeModification(s)"   = $($BaseResult.TestJobDuration);
-        "AfterBeModification(s)" = "$($testJob.TestJobDuration)";        
-        "% change" =  [System.Math]::Round(100 *( $($BaseResult.TestJobDuration) - $($testJob.TestJobDuration)) / $($BaseResult.TestJobDuration),2);  
-        })
-
-        if( $ResultFilter -ne "Passed" ) {
-            $testCasesescw = $testCasesescw | Select-Object *,@{label="NumOfPassedTestCases"; Expression= {$testJob.NumOfPassedTestCases}},@{label="NumOfFailedTestCases"; Expression= {$testJob.NumOfFailedTestCases}}
-        }
-
-    $testCasesexeSummaryClasswise += $testCasesescw
-}
-
-$testCasesexeSummaryClasswise | ConvertTo-Json | Out-File "P:\POC\result\Compare.log" -Force
-$testCasesexeSummaryClasswise | Export-Csv -Path "P:\POC\result\Compare.csv" -NoTypeInformation
+Start-Process Excel -FilePath $fileName
