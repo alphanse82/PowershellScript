c1a78725ecde3cb59d5695ad124660a1593dad51
alphanse82 <RakeshKumarGupta@outlook.com>
2019-11-18 15:21:10 -0800

    Update dotnetInstallation.ps1

--- dotnetInstallation.ps1
+++ dotnetInstallation.ps1
@@ -1,5 +1,5 @@
 mkdir C:\\downloadFiles
 $sourceNugetExe = "https://dotnetwebsite.azurewebsites.net/download/dotnet-core/scripts/v1/dotnet-install.ps1"
 $targetNugetExe = "C:\\downloadFiles\\dotnet-install.ps1"
-Invoke-WebRequest $sourceNugetExe -OutFile $targetNugetExe > C:\\1115\\log.txt
+Invoke-WebRequest $sourceNugetExe -OutFile $targetNugetExe > C:\\downloadFiles\\log.txt
 C:\\downloadFiles\\dotnet-install.ps1 > C:\\downloadFiles\\log1.txt
