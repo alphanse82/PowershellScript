d1f6b96206ed0401417dc11b3174996f35dfaa39
alphanse82 <RakeshKumarGupta@outlook.com>
2019-11-19 16:11:10 -0800

    Update dotnetInstallation.ps1

--- dotnetInstallation.ps1
+++ dotnetInstallation.ps1
@@ -1,7 +1,10 @@
 Set-ExecutionPolicy Unrestricted -Force
 
+[System.Environment]::SetEnvironmentVariable('MULTI_LEVEL_LOOKUP', '0')
+
 mkdir C:\\downloadFiles
 $sourceNugetExe = "https://dotnetwebsite.azurewebsites.net/download/dotnet-core/scripts/v1/dotnet-install.ps1"
 $targetNugetExe = "C:\\downloadFiles\\dotnet-install.ps1"
-Invoke-WebRequest $sourceNugetExe -OutFile $targetNugetExe > C:\\downloadFiles\\log.txt
-C:\\downloadFiles\\dotnet-install.ps1 > C:\\downloadFiles\\log1.txt
+Invoke-WebRequest $sourceNugetExe -OutFile $targetNugetExe
+cd C:\\downloadFiles
+.\dotnet-install.ps1
