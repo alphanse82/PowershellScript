927bab2fd42d296eb04bae380629528434c0211d
alphanse82 <RakeshKumarGupta@outlook.com>
2019-11-26 00:11:02 -0800

    Update dotnetInstallation.ps1

--- dotnetInstallation.ps1
+++ dotnetInstallation.ps1
@@ -17,7 +17,7 @@ Get-ChildItem C:\\downloadFiles -Filter *.zip | Expand-Archive -DestinationPath 
 .\azcopy_windows_amd64_10.1.2\azcopy.exe copy "https://environmentsetup.blob.core.windows.net/servicefab/ServiceFabric.XCopyPackage.6.6.73-internal.zip?sp=r&st=2019-11-26T07:28:25Z&se=2019-12-31T15:28:25Z&spr=https&sv=2019-02-02&sr=b&sig=gVgFM6WRyyQiK1RkKUpog2TptxLE%2B0vp7X1VWLu3XYY%3D" C:\\downloadFiles\\sf.zip
 Get-ChildItem C:\\downloadFiles -Filter *.zip | Expand-Archive -DestinationPath C:\\downloadFiles -Force
 
-#C:\downloadFiles\ServiceFabric.XCopyPackage.6.6.73-internal\lib\ServiceFabric.XCopyPackage\InstallFabric.ps1 C:\downloadFiles\ServiceFabric.XCopyPackage.6.6.73-internal\lib\ServiceFabric.XCopyPackage\MicrosoftServiceFabric.Internal.6.6.73.0505.cab -AcceptEULA
+C:\downloadFiles\ServiceFabric.XCopyPackage.6.6.73-internal\lib\ServiceFabric.XCopyPackage\InstallFabric.ps1 C:\downloadFiles\ServiceFabric.XCopyPackage.6.6.73-internal\lib\ServiceFabric.XCopyPackage\MicrosoftServiceFabric.Internal.6.6.73.0505.cab -AcceptEULA
 
 #net localgroup ServiceFabricAdministrators /add cloudtest
 
