3510f35928841f4f97153c9a167c2c88a0fb004a
alphanse82 <RakeshKumarGupta@outlook.com>
2020-04-18 00:19:12 -0700

    Create RefreshEnvironmentVariable.ps1

---
+++ RefreshEnvironmentVariable.ps1
@@ -0,0 +1,13 @@
+# this is useful when in same PS session you need to refresh Eng variables
+function RefreshEnvrionmentVariables()
+{
+    foreach($level in "Machine","User") {
+        [Environment]::GetEnvironmentVariables($level).GetEnumerator() | % {
+           # For Path variables, append the new values, if they're not already in there
+            if($_.Name -match 'Path$') { 
+                $_.Value = ($((Get-Content "Env:$($_.Name)") + ";$($_.Value)") -split ';' | Select -unique) -join ';'
+            }
+            $_
+        } | Set-Content -Path { "Env:$($_.Name)" }
+    }
+}
